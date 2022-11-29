140ed9e4ce3d250be61dec8e4a7529b7cd121688
Dilion <20221937@DFEi.ie>
2022-11-29 15:41:19 +0000

    Start project

---
+++ appPizza/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("appPizza")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("DFEi")>
+<Assembly: AssemblyProduct("appPizza")>
+<Assembly: AssemblyCopyright("Copyright © DFEi 2022")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("e562a36d-1398-4eb3-b9a2-f05394153c28")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
